f5edcd4a16827f41b5b64cf6199271b810c697a3
Irvin <irvin.noyola19@tectijuana.edu.mx>
2022-08-29 14:13:44 -0700

    Correcion

--- FamiliaApp/views.py
+++ FamiliaApp/views.py
@@ -1,3 +1,4 @@
+from this import d
 from django.shortcuts import render
 from .models import Familiar
 from django.template import loader
@@ -7,17 +8,17 @@ def fam(request):
     padre=Familiar(nombre="Tomas", edad=42, fecha='1980-09-23')
     madre=Familiar(nombre="Olga", edad=40, fecha='1982-03-02')
     hermana=Familiar(nombre="Rebeca", edad=15, fecha='2006-01-24')
-    #padre.save()
-    #madre.save()
-    #hermana.save()
+    padre.save()
+    madre.save()
+    hermana.save()
 
     listnom=[padre.nombre, madre.nombre, hermana.nombre]
     listage=[padre.edad, madre.edad, hermana.edad]
     listdate=[padre.fecha, madre.fecha, hermana.fecha]
 
-    doc={'Nombres':listnom, 'Edades':listage, 'Fechas':listdate}
-
-    plantilla=loader.get_template('index.html') 
-    doc=plantilla.render(doc)
+    plantilla=loader.get_template("index.html")
+    dicc={'Nombres': listnom, 'Edades': listage, 'Fechas': listdate}
+    doc=plantilla.render(dicc)
 
     return HttpResponse(doc)
+
--- MVTIrvin/urls.py
+++ MVTIrvin/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from FamiliaApp.views import fam 
+from FamiliaApp.views import fam
 
 urlpatterns = [
     path('admin/', admin.site.urls),
